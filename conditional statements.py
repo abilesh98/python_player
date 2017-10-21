dfe2e493598933664bfc6735f7e4e60b1bdd92b2
abilesh98 <32951256+abilesh98@users.noreply.github.com>
2017-10-21 10:21:51 +0530

    Add files via upload

---
+++ conditional statements.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse001
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16cse001 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    a=10
+    if(a<10):
+        print("a is less than 10")
+    else:
+        print("a is greater than 10")
+
+if __name__ == '__main__':
+    main()
