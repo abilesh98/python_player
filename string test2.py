e26c4ea930e5b517c640ca6069e403f89d44cbff
abilesh98 <32951256+abilesh98@users.noreply.github.com>
2017-10-21 10:08:16 +0530

    Add files via upload

---
+++ string test2.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      16cse001
+#
+# Created:     20/10/2017
+# Copyright:   (c) 16cse001 2017
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+   mystring="hello from ashok"+"kumar"
+   print(mystring[4:8])
+
+if __name__ == '__main__':
+    main()
